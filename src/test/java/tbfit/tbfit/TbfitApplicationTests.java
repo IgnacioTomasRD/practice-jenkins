b8102f16afdf399a9f79de518e730c57f6833da8
IgnacioTomasRD <iruizdiaz@frba.utn.edu.ar>
2023-09-02 21:47:34 -0300

    First commit

---
+++ src/test/java/tbfit/tbfit/TbfitApplicationTests.java
@@ -0,0 +1,13 @@
+package tbfit.tbfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
